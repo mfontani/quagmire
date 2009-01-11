1e3f53c46eea470c27ca1b59a9a6b12d71d82723
Marco Fontani <mfontani@cpan.org>
2009-01-11 21:14:03 +0000

    Status contains ->turn for whose turn it is

--- lib/Quagmire/Encounter/Status.pm
+++ lib/Quagmire/Encounter/Status.pm
@@ -1,12 +1,14 @@
 package Quagmire::Encounter::Status;
 use Moose;
 use MooseX::Storage;
+use Quagmire::Entity;
 our $VERSION = '0.01';
 our $AUTHORITY = 'cpan:MFONTANI';
 with Storage('format' => 'YAML', 'io' => 'File');
 
 has 'filename' => (is => 'rw', isa => 'Str', default=>'', required=>1);
 has 'round'  => (is => 'rw', isa => 'Int', default=>0, required=>1);
+has 'turn' => (is=>'rw',isa=>'Maybe[Quagmire::Entity]',required=>1,default=>sub{undef});
 
 __PACKAGE__->meta->make_immutable;
 1;
